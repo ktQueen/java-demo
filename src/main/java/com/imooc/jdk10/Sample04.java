1073e9cbb4178ee5e393cf765e70a414dd04b4ec
康婷 <kangting1@xiaomi.com>
2026-01-20 21:56:10 +0800

    feat: 不可变集合

---
+++ src/main/java/com/imooc/jdk10/Sample04.java
@@ -0,0 +1,16 @@
+package com.imooc.jdk10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//不可变集合
+public class Sample04 {
+    public static void main(String[] args) {
+        final var list = new ArrayList<String>();
+        list.add("ABC");
+        list.add("BCD");
+        list.add("EFG");
+        List<String> copyList = List.copyOf(list);
+        copyList.add("A");
+    }
+}

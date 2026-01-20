c4b3a08c3d472e667a5a59d2333bfd9307aa7cd4
康婷 <kangting1@xiaomi.com>
2026-01-20 21:52:59 +0800

    feat: 下载器

--- src/main/java/com/imooc/downloader/Downloader.java
+++ src/main/java/com/imooc/downloader/Downloader.java
@@ -3,11 +3,107 @@ package com.imooc.downloader;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Downloader {
+    private Integer threadNum = 10;
+
     public static void main(String[] args) {
         Downloader downloader = new Downloader();
-        downloader.download("https://pic1.zhimg.com/v2-6b6a983b42742a373670b8b85c16c2ae.jpg", "/Users/kangting/Desktop/2025-kt");
+        downloader.start("/Users/kangting/Desktop/java-project/java-demo/src");
+//        downloader.download("https://pic1.zhimg.com/v2-6b6a983b42742a373670b8b85c16c2ae.jpg", "/Users/kangting/Desktop/2025-kt");
+    }
+
+    /**
+     * 从指定文件读取下载地址，批量下载网络资源
+     *
+     * @param targetDir   下载文件的存储目录
+     * @param downloadTxt download.txt完整路径
+     */
+    public void multiDownloadFromFile(String targetDir, String downloadTxt) {
+        File dir = new File(targetDir);
+        //新建目标目录
+        if (!dir.exists()) {
+            dir.mkdirs();
+            System.out.println("[INFO]发现下载目录[" + dir.getPath() + "]不存在，已自动创建");
+        }
+        //读取download.txt 存入resources集合
+        List<String> resources = new ArrayList<>();
+        BufferedReader reader = null;
+        ExecutorService threadPool = null;
+        try {
+            reader = new BufferedReader(new FileReader(downloadTxt));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                resources.add(line);
+                System.out.println(line);
+            }
+            threadPool = Executors.newFixedThreadPool(this.threadNum);
+            Downloader that = this;
+            for (String res : resources) {
+                threadPool.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        that.download(res, targetDir);
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (threadPool != null) {
+                threadPool.shutdownNow();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 开始多线程下载
+     *
+     * @param propDir config.properties 所在目录
+     */
+    public void start(String propDir) {
+        //指定config.properties完整路径
+        File propFile = new File(propDir + "/config.properties");
+        //实例化Properties属性类
+        Properties properties = new Properties();
+        Reader reader = null;
+        try {
+            //实例化FileReader对象
+            reader = new FileReader((propFile));
+            //properties对象读取配置文件
+            properties.load(reader);
+            //通过getProperty方法得到对应的选项值
+            String threadNum = properties.getProperty("thread-num");
+            this.threadNum = Integer.parseInt(threadNum);
+            String targetDir = properties.getProperty("target-dir");
+            //打印输出
+//            System.out.println(threadNum);
+//            System.out.println(targetDir);
+            this.multiDownloadFromFile(targetDir, propDir + "/download.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    //关闭流
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     /**

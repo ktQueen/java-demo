356078cb432541730b4e1e376c38dccbc9b370b3
康婷 <kangting1@xiaomi.com>
2026-02-10 17:08:51 +0800

    feat: 使用 Apache Commons DBUtils 结合 Druid 实现插入

---
+++ src/main/java/com/imooc/goods/Goods.java
@@ -0,0 +1,70 @@
+package com.imooc.goods;
+
+import java.util.Date;
+
+public class Goods {
+    private Integer id;
+    private String name;
+    private Double price;
+    private String desp;
+    private Date create_time;
+
+    public Goods() {
+    }
+
+    public Goods(Integer id, String name, Double price, String desp, Date create_time) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.desp = desp;
+        this.create_time = create_time;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDesp() {
+        return desp;
+    }
+
+    public void setDesp(String desp) {
+        this.desp = desp;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods [name=" + name +
+                ",price=" + price +
+                ",desp=" + desp +
+                ",create_time=" + create_time + "]";
+    }
+}
---
+++ src/main/java/com/imooc/goods/GoodsDbUtilsDemo.java
@@ -0,0 +1,36 @@
+package com.imooc.goods;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+import javax.sql.DataSource;
+import java.sql.Date;
+import java.util.List;
+
+public class GoodsDbUtilsDemo {
+    public static void main(String[] args) throws Exception {
+        DataSource ds = DruidUtil.getDataSource();
+        QueryRunner runner = new QueryRunner(ds);
+
+        //1.插入一条“照相机 5000.0防水，索尼1999-01-30”
+        String insertSql = "INSERT INTO goods(name,price,desp,create_time) VALUES (?,?,?,?)";
+        Object[] params = {
+                "照相机",
+                5000.0,
+                "防水，索尼",
+                Date.valueOf("1999-01-30")
+        };
+        int rows = runner.update(insertSql, params);
+        if (rows > 0) {
+            System.out.println("添加成功");
+            //2.查询所有数据并打印
+            String querySql = "SELECT id,name,price,desp,create_time FROM goods";
+            List<Goods> list = runner.query(querySql, new BeanListHandler<>(Goods.class));
+            for (Goods g : list) {
+                System.out.println(g);
+            }
+        } else {
+            System.out.println("添加失败");
+        }
+    }
+}

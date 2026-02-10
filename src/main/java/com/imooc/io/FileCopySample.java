1304e9b2cca5769a004857fc06541706e0b2ec3f
康婷 <kangting1@xiaomi.com>
2026-02-10 16:48:28 +0800

    feat: 数据流注释

--- src/main/java/com/imooc/io/FileCopySample.java
+++ src/main/java/com/imooc/io/FileCopySample.java
@@ -11,11 +11,14 @@ public class FileCopySample {
         try {
             //实例化InputStream对象
             fis = new FileInputStream(source);
+            //实例化Outputstream对象
             fos = new FileOutputStream(target);
             byte[] bs = new byte[1024];
             int len;
+            //利用read方法循环读取的字节数据，并进行处理
             while ((len = fis.read(bs)) != -1) {
                 System.out.println(len);
+                //将读取到字节数组写入到输出流
                 fos.write(bs, 0, len);
             }
         } catch (FileNotFoundException e) {
